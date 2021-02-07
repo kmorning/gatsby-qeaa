10a58d3c4646bb52dbec78657974cbc1a9a0f624
Kelly Morning <kleemorning@gmail.com>
2021-02-06 19:27:57 -0500

    upstream typescript register

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+'use strict';
 
-module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+/**
+ * Run TypeScript code without compiling it
+ * Source-map-support mimics node's stack trace making debugging easier
+ * ts-node register helps compiling and importing TypeScript modules
+ */
+require('source-map-support').install();
+require('ts-node').register();
+
+module.exports = require('./gatsby-config.ts');
---
+++ gatsby-config.ts
@@ -0,0 +1,8 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+export default {
+  /* Your site config here */
+}
