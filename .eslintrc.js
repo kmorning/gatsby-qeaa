18fe2858f78e1cf1b593df88526769e4ce5bd2db
Kelly Morning <kleemorning@gmail.com>
2021-02-06 23:37:56 -0500

    change eslint/prettier preferences

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,54 @@
 module.exports = {
-    parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-    extends: [
-        "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
-        "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
-        "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-        "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    ],
-    parserOptions: {
-        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-        sourceType: "module", // Allows for the use of imports
-        ecmaFeatures: {
-            jsx: true // Allows for the parsing of JSX
-        }
-    },
-    rules: {
-        // Place to specify ESLint rules.
-        '@typescript-eslint/interface-name-prefix': ['error', { prefixWithI: 'always' }],
-        'react/prop-types': 'off',
-        '@typescript-eslint/ban-ts-ignore': 'off'
-    },
-    settings: {
-        react: {
-            version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
-        }
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    // Uses the recommended rules from @eslint-plugin-react
+    'plugin:react/recommended',
+    // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'plugin:@typescript-eslint/recommended',
+    // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'prettier/@typescript-eslint',
+    // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors.
+    // Make sure this is always the last configuration in the extends array.
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
     }
-};
+  },
+  rules: {
+    // Place to specify ESLint rules.
+    //'@typescript-eslint/interface-name-prefix': ['error', { prefixWithI: 'always' }],
+    //'react/prop-types': 'off',
+    //'@typescript-eslint/ban-ts-ignore': 'off'
+    'max-len': [
+      'error',
+      {
+        code: 140,
+        ignoreUrls: true
+      }
+    ],
+    'react/no-unescaped-entities': [
+      'error',
+      {
+        forbid: [
+          {
+            char: '>',
+            alternatives: ['&gt;']
+          },
+          {
+            char: '}',
+            alternatives: ['&#125;']
+          }
+        ]
+      }
+    ]
+  },
+  settings: {
+    react: {
+      version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
+    }
+  }
+}
