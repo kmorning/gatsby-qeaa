afdd683e37dc487e1f9c5c083c0e5d9aa204818f
Kelly Morning <kleemorning@gmail.com>
2021-02-06 23:40:27 -0500

    use react FC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import React from "react"
+import React from 'react'
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const Home: React.FC = () => <div>Hello world!</div>
+
+export default Home
